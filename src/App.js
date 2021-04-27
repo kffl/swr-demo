b6ed69f54eeeb420a5e2ee4ad7cb0c7515509a16
kffl <pawel@kuffel.io>
2021-04-27 16:18:12 +0200

    SWR + optimistic UI rendering

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-//const fetcher = url => fetch("http://localhost:5000" + url).then(r => r.json())
+const fetcher = (url) =>
+    fetch("http://localhost:5000" + url).then((r) => r.json());
 
 function App() {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        fetch("http://localhost:5000").then(async (res) => {
-            const d = await res.json();
-            setData(d);
-            setLoading(false);
-        });
-    }, []);
+    const { data, error, mutate } = useSWR('/', fetcher);
+
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p
                     onClick={() => {
+                        const todoName = Math.random()
+                            .toString(36)
+                            .substring(7);
+                        mutate(
+                            [
+                                ...data,
+                                { id: Date.now().toString(), name: todoName },
+                            ],
+                            false
+                        );
                         fetch("http://localhost:5000", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json",
                             },
                             body: JSON.stringify({
-                                name: Math.random().toString(36).substring(7),
+                                name: todoName,
                             }),
-                        }).then();
+                        }).then(() => {
+                            mutate();
+                        });
                     }}
                 >
                     Add todo
                 </p>
-                {loading && <p>Loading...</p>}
+                {!data && !error && <p>Loading...</p>}
                 {data && (
                     <ol>
                         {data.map((todo) => (
